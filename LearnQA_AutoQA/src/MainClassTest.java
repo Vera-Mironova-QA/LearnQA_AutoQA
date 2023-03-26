3b84a26d1dcf848b8bf94ac323454d7acfbafbcf
Vera Ivanova <vera.ivanova@sbermarket.ru>
2023-03-26 18:39:57 +0400

    Second test issue commit

--- LearnQA_AutoQA/src/MainClassTest.java
+++ LearnQA_AutoQA/src/MainClassTest.java
@@ -3,8 +3,14 @@ import org.junit.Test;
 
 public class MainClassTest {
     MainClass Main = new MainClass();
+
     @Test
     public void testGetLocalNumber() {
-            Assert.assertTrue("Method getLocalNumber return NOT 14",Main.getLocalNumber() == 14);
-        }
+        Assert.assertTrue("Method getLocalNumber return NOT 14", Main.getLocalNumber() == 14);
     }
+
+    @Test
+    public void testClassNumber() {
+        Assert.assertTrue("Class number less  or equal 45",Main.getClassNumber() > 45);
+    }
+}
